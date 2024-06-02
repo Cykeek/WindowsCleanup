116bbd2809cfd5cfbc4508e43719179648b1034c
Cykeek <Cykeek@proton.me>
2024-06-02 18:36:52 +0530

    Add popup after the files gets executed and also remove the automatic logs.txt gets opened we don't need that for user side.
    That's for developer!!
    
    - also don't log network related in logs.txt
      we don't need it. Useless anyway
    
    Signed-off-by: Cykeek <Cykeek@proton.me>

---
+++ popup.vbs
@@ -0,0 +1,2 @@
+Set objShell = CreateObject("WScript.Shell")
+objShell.Popup "Temporary files have been cleaned successfully", 5, "Cleanup Completed", 64
--- runme.bat
+++ runme.bat
@@ -41,20 +41,17 @@ REM Ping www.google.com four times
 echo Pinging www.google.com...
 ping www.google.com -n 4 | findstr /C:"TTL=" > nul
 if errorlevel 1 (
-    echo Network is disconnected.
+    echo Network is disconnected. >> logs.txt
 ) else (
-    echo Network is connected.
+    echo Network is connected. >> logs.txt
 )
 
 echo.
 
 echo Temporary files have been cleaned and network connectivity has been checked. Press any key to exit.
 
-REM Open logs.txt
-start "" "logs.txt"
-
-REM Countdown before exiting
-timeout /t 5 >nul
+REM Run popup.vbs to show the message
+wscript.exe "popup.vbs"
 
 REM Close Command Prompt window
 exit
