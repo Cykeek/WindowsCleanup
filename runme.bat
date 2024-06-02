d0d8d8795128707e87ee39507b52e4018cf8ff3e
Cykeek <Cykeek@proton.me>
2024-06-02 19:22:47 +0530

    Fix Bugs
    - While executing the script as Admin access it can't find the "popup.vbs" script
    - Added admin access check and prompt
    - Improved code readability and added comments
    - Added support for Cleaning cache files of chrome, firefox, edge
    
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- runme.bat
+++ runme.bat
@@ -1,15 +1,31 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM Get the directory where this script is located
+set "SCRIPT_DIR=%~dp0"
+
 REM Delete previous logs.txt if exists
-if exist logs.txt (
-    del logs.txt
+if exist "%SCRIPT_DIR%logs.txt" (
+    del "%SCRIPT_DIR%logs.txt"
 )
 
-REM Prompt for admin rights
-echo Administrative privileges are required to clean temporary files and perform network checks.
-echo Please provide admin credentials if prompted.
-echo.
+REM Check if script is running with admin privileges
+NET FILE 1>NUL 2>NUL
+if %errorlevel% == 0 (
+    set "ADMIN_ACCESS=1"
+) else (
+    set "ADMIN_ACCESS=0"
+)
+
+REM If not running with admin privileges, prompt for admin rights
+if %ADMIN_ACCESS% == 0 (
+    echo Administrative privileges are required to clean temporary files and perform network checks.
+    echo Please provide admin credentials if prompted.
+    echo.
+)
+
+REM Change the working directory to the script directory
+cd /d "%SCRIPT_DIR%"
 
 REM Clean temporary folders
 echo Cleaning temporary folders...
@@ -27,10 +43,29 @@ echo Cleaning prefetch folder... >> logs.txt
 del /q /f /s %windir%\prefetch\*.* >> logs.txt 2>>&1
 echo.
 
-REM Clean temp folder
-echo Cleaning temp folder... >> logs.txt
-del /q /f /s %windir%\temp\*.* >> logs.txt 2>>&1
-echo.
+REM Check if Chrome is installed
+if exist "%LocalAppData%\Google\Chrome\User Data\Default\Cache\" (
+    REM Clean temp folder for Chrome
+    echo Cleaning Chrome cache files... >> logs.txt
+    del /q /f /s "%LocalAppData%\Google\Chrome\User Data\Default\Cache\*.*" >> logs.txt 2>>&1
+    echo.
+)
+
+REM Check if Edge is installed
+if exist "%LocalAppData%\Microsoft\Edge\User Data\" (
+    REM Clean temp folder for Edge
+    echo Cleaning Edge cache files... >> logs.txt
+    del /q /f /s "%LocalAppData%\Microsoft\Edge\User Data\Default\Cache\*.*" >> logs.txt 2>>&1
+    echo.
+)
+
+REM Check if Firefox is installed
+if exist "%AppData%\Mozilla\Firefox\Profiles\" (
+    REM Clean temp folder for Firefox
+    echo Cleaning Firefox cache files... >> logs.txt
+    del /q /f /s "%LocalAppData%\Mozilla\Firefox\Profiles\*\cache2\*.*" >> logs.txt 2>>&1
+    echo.
+)
 
 REM Check network connectivity
 echo Checking network connectivity...
@@ -51,7 +86,7 @@ echo.
 echo Temporary files have been cleaned and network connectivity has been checked. Press any key to exit.
 
 REM Run popup.vbs to show the message
-wscript.exe "popup.vbs"
+wscript.exe "%SCRIPT_DIR%popup.vbs"
 
 REM Close Command Prompt window
 exit
