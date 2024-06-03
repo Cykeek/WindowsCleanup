0707618bca7a6423c0e39d70a620c5213c13b919
Cykeek <Cykeek@proton.me>
2024-06-03 19:07:28 +0530

    Add More Knows Processes
    - and remove startup processhack.bat script we don't need that
      as we already adding it in task scheduler.
    
    - Add more better error handling
    
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- processhack/processhack.bat
+++ processhack/processhack.bat
@@ -25,19 +25,37 @@ REM Idle priority [Values = 64]
 setlocal EnableDelayedExpansion
 set "TARGET_PROCESSES[0]=chrome.exe"
 set "TARGET_PROCESSES[1]=cs2.exe"
-set "TARGET_PROCESSES[2]=notepad.exe"
-set "DESIRED_PRIORITIES[0]=32"
+set "TARGET_PROCESSES[2]=cmd.exe"
+set "TARGET_PROCESSES[3]=steam.exe"
+set "TARGET_PROCESSES[4]=SteamService.exe"
+set "TARGET_PROCESSES[5]=RiotClientServices.exe"
+set "TARGET_PROCESSES[6]=RiotClientCrashHandler.exe"
+set "TARGET_PROCESSES[7]=PhoneExperienceHost.exe"
+set "TARGET_PROCESSES[8]=OfficeClickToRun.exe"
+set "TARGET_PROCESSES[9]=msedgewebview2.exe"
+set "TARGET_PROCESSES[10]=MicrosoftEdgeUpdate.exe"
+set "TARGET_PROCESSES[11]=Discord.exe"
+set "DESIRED_PRIORITIES[0]=16384"
 set "DESIRED_PRIORITIES[1]=256"
-set "DESIRED_PRIORITIES[2]=32"
+set "DESIRED_PRIORITIES[2]=64"
+set "DESIRED_PRIORITIES[3]=64"
+set "DESIRED_PRIORITIES[4]=64"
+set "DESIRED_PRIORITIES[5]=64"
+set "DESIRED_PRIORITIES[6]=64"
+set "DESIRED_PRIORITIES[7]=64"
+set "DESIRED_PRIORITIES[8]=64"
+set "DESIRED_PRIORITIES[9]=64"
+set "DESIRED_PRIORITIES[10]=64"
+set "DESIRED_PRIORITIES[11]=64"
 
 REM Initialize process states
-for /L %%i in (0,1,2) do (
+for /L %%i in (0,1,11) do (
     set "PROCESS_STATE[%%i]=not running"
 )
 
 :ProcessHack
 REM Iterate through the array of target processes
-for /L %%i in (0,1,2) do (
+for /L %%i in (0,1,11) do (
     set "TARGET_PROCESS=!TARGET_PROCESSES[%%i]!"
     set "DESIRED_PRIORITY=!DESIRED_PRIORITIES[%%i]!"
 
@@ -49,7 +67,11 @@ for /L %%i in (0,1,2) do (
             echo [!DATE! !TIME!] !TARGET_PROCESS! started. >> "%SCRIPT_DIR%logging\process_logs.txt"
             echo !TARGET_PROCESS! detected and running.
             wmic process where "name='!TARGET_PROCESS!'" CALL setpriority !DESIRED_PRIORITY! > nul
-            echo [!DATE! !TIME!] Priority of process !TARGET_PROCESS! set to !DESIRED_PRIORITY!. >> "%SCRIPT_DIR%logging\process_logs.txt"
+            if !errorlevel! equ 0 (
+                echo [!DATE! !TIME!] Failed to set priority of process !TARGET_PROCESS!. >> "%SCRIPT_DIR%logging\process_logs.txt"
+            ) else (
+                echo [!DATE! !TIME!] Priority of process !TARGET_PROCESS! set to !DESIRED_PRIORITY!. >> "%SCRIPT_DIR%logging\process_logs.txt"
+            )
             set "PROCESS_STATE[%%i]=running"
         )
     ) else (
